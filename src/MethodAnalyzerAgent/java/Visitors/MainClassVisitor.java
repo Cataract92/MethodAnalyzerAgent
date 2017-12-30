753d77870b9a47f8ff9b0e94be96c32f4b6f8b02
Cataract <salatgurke92@hotmail.de>
2017-12-31 00:29:11 +0100

    implemented MethodAnalyzerAgent

---
+++ src/MethodAnalyzerAgent/java/Visitors/MainClassVisitor.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017.
+ * Nico Feld
+ * 1169233
+ */
+
+package Visitors;
+
+import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.MethodVisitor;
+import jdk.internal.org.objectweb.asm.Opcodes;
+
+public class MainClassVisitor extends ClassVisitor implements Opcodes
+{
+    private static final int API_VERSION = ASM5;
+    public MainClassVisitor(ClassVisitor cv) {
+        super(API_VERSION, cv);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String
+            desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc,
+                signature, exceptions);
+        return new MainMethodVisitor(API_VERSION, mv, access, name,
+                desc);
+    }
+}
