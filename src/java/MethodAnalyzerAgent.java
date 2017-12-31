5c693ab998f541e351ee389b4e6ee7cfb996555c
Cataract <s4fenico@uni-trier.de>
2017-12-31 01:01:43 +0100

    reworked

---
+++ src/java/MethodAnalyzerAgent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017.
+ * Nico Feld
+ * 1169233
+ */
+
+/*
+    Usage:
+        Add as a VM parameter: -javaagent:{PathToJar}={TargetClassName}
+        eg.: -javaagent:/home/fst/MethodAnalyzerAgent.jar=SDRaytracer
+ */
+
+import Injection.AnalyzingHandler;
+import Visitors.MainClassVisitor;
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.ClassWriter;
+import java.lang.instrument.Instrumentation;
+
+public class MethodAnalyzerAgent {
+
+
+    public static void premain(String agentArgs,
+                               Instrumentation inst) {
+
+        AnalyzingHandler.targetClassName = agentArgs;
+
+        if (agentArgs == null)
+            AnalyzingHandler.targetClassName = "SDRaytracer";
+
+        inst.addTransformer((loader, className, classBeingRedefined, protectionDomain, classfileBuffer) ->
+        {
+            if (AnalyzingHandler.targetClassName.equals(className)) {
+                try {
+                    ClassReader classReader = new ClassReader(classfileBuffer);
+                    ClassWriter classWriter = new ClassWriter(classReader,
+                            ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+                    ClassVisitor mainClassVisitor = new
+                            MainClassVisitor(classWriter);
+                    classReader.accept(mainClassVisitor,
+                            ClassReader.EXPAND_FRAMES);
+                    return classWriter.toByteArray();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println(e.getMessage());
+                    return classfileBuffer;
+                } }
+
+
+
+            return classfileBuffer;
+        });
+    }
+}
